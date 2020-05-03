c14cefdc3677fca3e200b77dbc8dcfae0a3b3a06
russ <russ@tidepool.org>
2020-05-03 09:54:07 -0500

    fix : file path for test

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,9 +7,9 @@ from pathlib import Path
 
 @pytest.fixture
 def bg_array(scope="module"):
-    print("this path : ", Path(__file__).parent.resolve())
-    print("path is : ", os.getcwd())
-    return np.genfromtxt("../bg_array.csv", delimiter=",")
+    return np.genfromtxt(
+        f"{Path(__file__).parent.resolve()}/bg_array.csv", delimiter=","
+    )
 
 
 @pytest.fixture
@@ -20,5 +20,5 @@ def bg_values__3_values(scope="module"):
 @pytest.fixture
 def get_date_ep_array(scope="module"):
     print("path is : ", os.getcwd())
-    df = pd.read_csv("../date_ep_values.csv")
+    df = pd.read_csv(f"{Path(__file__).parent.resolve()}/date_ep_values.csv")
     return df.to_numpy(dtype=object)
